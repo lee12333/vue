6093a7ac7bb0f341b63e847d330ec03df1c52f0a
lee12333 <dlwhdwls1215@daum.net>
2023-07-12 11:37:56 +0900

    완성코드

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+      <h1>번호 저장</h1>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+    h1 {
+      color: rgb(91, 167, 56);
+      font-weight: 900;
+      margin: 2.5rem 0 1.5rem;
+    }
+  </style>
